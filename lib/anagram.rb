531c33a2e3f9d69dda9c468303c54695eb73e6a9
ryanadams <rcoleman.adams@gmail.com>
2022-05-23 18:03:24 -0500

    commit tree ea85fc7a13e77204b2b417d0238be575f15fca1d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def match(array)
+    letters = @name.chars
+     array.filter do |word|
+       word.chars.sort == letters.sort
+     end
+   end
+
+end
